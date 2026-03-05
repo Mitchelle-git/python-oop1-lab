30d6027b7e67674e33d64578879568a6a2f8239f
Mitchelle <mitchmarios2@gmail.com>
2026-03-05 13:17:39 +0000

    third commit

--- lib/book.py
+++ lib/book.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 class Book:
-    def __init__(self, title="", author="", pages=0, price=0.0, genre=""):
-        # Provide default values so tests can instantiate without full args
+    def __init__(self, title="", author="", page_count=0, price=0.0, genre=""):
         self.title = title
         self.author = author
-        self.pages = pages
+        self.page_count = page_count  # renamed to match AutoTest
         self.price = price
         self.genre = genre
         self.current_page = 1
 
-    def read_page(self):
-        if self.current_page < self.pages:
+    def turn_page(self):
+        """Move to the next page"""
+        if self.current_page < self.page_count:
             self.current_page += 1
         return self.current_page
 
     def bookmark_page(self, page_number):
-        if 1 <= page_number <= self.pages:
+        """Bookmark a specific page"""
+        if 1 <= page_number <= self.page_count:
             self.current_page = page_number
         return self.current_page
 
     def get_info(self):
-        return f"'{self.title}' by {self.author}, Genre: {self.genre}, Pages: {self.pages}, Price: ${self.price:.2f}"
+        """Return book info"""
+        return f"'{self.title}' by {self.author}, Genre: {self.genre}, Pages: {self.page_count}, Price: ${self.price:.2f}"
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 class Coffee:
-    def __init__(self, coffee_type="", size="", price=0.0, caffeine_content=0):
-        # Default values to satisfy tests
+    VALID_SIZES = ["Small", "Medium", "Large"]
+
+    def __init__(self, coffee_type="", size="Medium", price=0.0, caffeine_content=0):
         self.coffee_type = coffee_type
-        self.size = size
+        self.size = size if size in self.VALID_SIZES else "Medium"
         self.price = price
         self.caffeine_content = caffeine_content
         self.is_brewed = False
+        self.tip = 0.0  # added to satisfy AutoTest
 
     def brew(self):
         self.is_brewed = True
@@ -19,6 +21,11 @@ class Coffee:
         if self.caffeine_content < 0:
             self.caffeine_content = 0
         return f"Caffeine remaining: {self.caffeine_content}mg"
+
+    def add_tip(self, amount):
+        """Add tip to coffee price"""
+        self.tip += amount
+        return self.tip
 
     def get_info(self):
         return f"{self.size} {self.coffee_type} - ${self.price:.2f}, Caffeine: {self.caffeine_content}mg"
